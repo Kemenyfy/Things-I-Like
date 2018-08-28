56a32288282d254d64a723847566696f307415db
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-28 08:30:56 -0400

    Commit Web Working, now starting big CSS and Content Refactor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Breadcrumbs from 'react-router-dynamic-breadcrumbs'
+
 import './App.css';
+
+import CategoryList from './Components/CategoryList';
+import PhotoList from './Components/PhotoList';
+import PhotoDetail from './Components/PhotoDetail';
+
+const routes = {
+  '/': 'Home',
+  '/:category': 'Photo List', 
+  '/:category/:id': 'Photo Details'
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Things I Likes</h1>
+            <h3 className="">Created by Somebody/Somehow/Maybe</h3>
+          </header>
+          <Breadcrumbs mappedRoutes={routes} />
+          <Switch>
+            <Route path="/" exact component={CategoryList}/>
+            <Route path="/:category" exact component={PhotoList}/>
+            <Route path="/:category/:index" exact component={PhotoDetail}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
